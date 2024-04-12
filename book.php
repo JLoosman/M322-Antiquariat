c8a3f47a922c9ea561c819ca424efafd9ca104b9
JLoosman <Jesiah.Loosman@outlook.de>
2024-04-12 18:01:44 +0200

    Added more data to book details page

--- book.php
+++ book.php
@@ -8,24 +8,54 @@
     <link href="css/book.css" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Document</title>
+    <title>Details</title>
 </head>
 <body>
     <?php include("header.php"); ?>
     <?php
+    function test_input($data) {
+        if(is_array($data)) {
+            $arr = [];
+            foreach ($data as $dataset) {
+                array_push($arr, test_input($dataset));
+            }
+            return $arr;
+        } else {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+    }
+
     if(isset($_GET["id"])) {
 
         include("connection.php");
         /** @var TYPE_NAME $conn */
         $statement = $conn->prepare("SELECT * FROM buecher WHERE id = :id");
 
-        $statement->execute(["id" => $_GET['id']]);
+        $statement->execute(["id" => test_input($_GET['id'])]);
 
         $row = $statement->fetch();
 
         $title = $row["kurztitle"];
-        $author = $row["autor"];
+        $author = ($row["autor"] == " " ? "Kein Autor bekannt" : $row["autor"]);
         $description = $row["title"];
+        $available = $row["verkauft"];
+        $category = $row["kategorie"];
+
+        if ($category <= 13) {
+            $statement = $conn->prepare("SELECT * FROM kategorien WHERE id = :kategorieID");
+
+            $statement->execute(["kategorieID" => $category]);
+
+            $categoryRow = $statement->fetch();
+
+            $category = $categoryRow["kategorie"];
+        } else {
+            $category = "Kategorie unbekannt";
+        }
+
     } else {
         header("Location: katalog.php");
     }
@@ -37,8 +67,14 @@
                 <circle cx="338" cy="338" r="338" fill="#D9D9D9"/>
         </div>
         <div class="right-content">
-            <h1><?=$title?></h1>
-            <h3><?=$author?></h3>
+            <div class="top">
+                <h1><?=$title?></h1>
+                <?= $available ? '<i class="unavailable fa-regular fa-circle-xmark"></i>': '<i class="available fa-regular fa-circle-check"></i>'?>
+            </div>
+            <div class="mid">
+                <h3><?=$author?></h3>
+                <h3 class="category" ><?=$category?></h3>
+            </div>
             <div class="rating">
                 <i class="fa-regular fa-star"></i>
                 <i class="fa-regular fa-star"></i>
